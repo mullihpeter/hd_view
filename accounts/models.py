e12613542edc4b91843f238a4483d68d19d64294
mullihpeter <108232385+mullihpeter@users.noreply.github.com>
2023-01-30 15:22:26 +0300

    Update models.py

--- accounts/models.py
+++ accounts/models.py
@@ -12,11 +12,14 @@ AGE_CHOICES = (
 
 class CustomUser(AbstractUser):
     profiles = models.OneToOneField('Profile', blank=True)
-    email = models.EmailField(blank=True, unique=True)
+    username = models.CharField(max_length=350)
+    email = models.EmailField(blank=False,max_length=254, verbose_name="email address")
+    
+    USERNAME_FIELD = "username"
+    EMAIL_FIELD = "email"
 
 
 class Profile(models.Model):
-    username = models.CharField(max_length=350)
     age_limit = models.CharField(choices=AGE_CHOICES, max_length=15)
     profile_pic = models.ImageField(upload_to='profile_pic', default='default.png')
     uuid = models.UUIDField(default=uuid.uuid4)
